fd5212eb242a8733e913b3698d05d0310fd8f8cb
DrUSB <salimia2@uni.coventry.ac.uk>
2017-02-03 10:40:16 +0000

    Updated help menu
    Added a function to note if they type help and to display commands they
    can use.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+
 using namespace std;
 
 int main(){
-
+    string checkForHelp;
     cout << "Welcome to Galactic StrongHold!" << endl;
     cin.get();
     cout << "Game is now loading..."<< endl;
     cin.get();
     cout << "Type help for more information on what commands to use!"<< endl;
+    cin >> checkForHelp;
+    if (checkForHelp.compare("help") == 0){
+        cout << "**********-------------Help Menu--------------**********\n***Movements:***\nMove North\nMove East\nMove South\nMove West" << endl;
+    }
+
     return 0;
 }
