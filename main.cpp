7c0e773d9c4a1dc1b8284e34c4123780817436f0
DrUSB <salimia2@uni.coventry.ac.uk>
2017-02-03 10:03:44 +0000

    Basic start

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Welcome to Galactic StrongHold!" << endl;
+    cin.get();
+    cout << "Game is now loading..."<< endl;
+    cin.get();
+    cout << "Type help for more information on what commands to use!"<< endl;
+    return 0;
+}
