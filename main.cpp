ac15892eb03b472316bdbb69731b663bfd25a915
DrUSB <salimia2@uni.coventry.ac.uk>
2017-02-06 10:59:43 +0000

    Updated menus

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     string checkForHelp;
-    cout << "Welcome to Galactic StrongHold!" << endl;
+    cout << "Welcome to GalAcTicTRaz!" << endl;
     cin.get();
     cout << "Game is now loading..."<< endl;
     cin.get();
@@ -12,9 +12,36 @@ int main(){
     cin >> checkForHelp;
     if (checkForHelp.compare("help") == 0){
         cout << "************************************************\n"
-                "                   Help Menu\n************************************************\n"
-                "\nMovements:\n\nmoveN        Moves to North room\nmoveE        Moves to East room\nmoveS        Moves to South room\nmoveW        Moves to West room\nsearch       Searches local room\ntalk         Talks if possible to person in room\ninfo         Gives information on the room you're in"
-                "\n\n***Character Abilities***\n " << endl;
+                "                   Help Menu\n"
+                "************************************************\n"
+                "\nMovements:\n\nn                Moves to North room\n"
+                "e                Moves to East room\n"
+                "s                Moves to South room\n"
+                "w                Moves to West room\n\n"
+                "Character Abilities:\n\n"
+                "search                         Searches local room\n"
+                "look                           Looks around room\n"
+                "talk                           Talks if possible to person in room\n"
+                "info                           Gives information on the room you're in\n"
+                "pickUp(item)                   Pickes up specified item\n"
+                "kill(enemy) with (item)        Kills a specified enemy with a chosen item\n"
+                "break(object)                  Breaks chosen object in room\n"
+                "open(oject)                    Open specified object e.g Box,Crate\n"
+                "drop(object)                   Drops specified item from inventory\n"
+                "examine(object)                Examines specified object\n"
+                "\n"
+                "Game options:\n"
+                "save                           Saves game at current point\n"
+                "restore                        Return to last save\n"
+                "restart                        Restarts game at beginning\n"
+                "quit                           Quits game\n"
+                "\n"
+                "Class/Crime Abilities:\n"
+                "Thief:\n"
+                "picklock(object)               Picklock a door or chest etc.\n"
+                "stealth                        Toggle stealth to go into or out off stealth mode\n"
+
+                 << endl;
     }
 
     return 0;
