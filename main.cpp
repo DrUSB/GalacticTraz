81c0c931cedc4100e8b10469fc1df387d6ee9449
DrUSB <soheil.salimian@hotmail.co.uk>
2017-02-08 17:36:10 +0000

    option to choose race and finished help menu
    Option to choose race and finished help menu

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
+#include <conio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(){
+    string name;
+    int race;
+    int cl4ss;
     string checkForHelp;
     cout << "Welcome to GalAcTicTRaz!" << endl;
     cin.get();
     cout << "Game is now loading..."<< endl;
     cin.get();
-    cout << "Type help for more information on what commands to use!"<< endl;
+    cout << "For information on commands type help. Otherwise type anything to continue" << endl;
     cin >> checkForHelp;
     if (checkForHelp.compare("help") == 0){
         cout << "************************************************\n"
                 "                   Help Menu\n"
                 "************************************************\n"
-                "\nMovements:\n\nn                Moves to North room\n"
+                "\nMovements:\n\n"
+                "n                Moves to North room\n"
                 "e                Moves to East room\n"
                 "s                Moves to South room\n"
                 "w                Moves to West room\n\n"
@@ -40,9 +46,26 @@ int main(){
                 "Thief:\n"
                 "picklock(object)               Picklock a door or chest etc.\n"
                 "stealth                        Toggle stealth to go into or out off stealth mode\n"
+                "Murderer:\n"
+                "smash(object)                  Smashes chosen object for example a door\n"
+                "charge(enemy)                  Charges enemy dealing damage same as weapon held + 30%\n\n"
+                "Con Artist:\n"
+                "teleport(direction)            Works instead of movement but can go past locked doors\n"
+                "invisible                      Makes you go invisible\n" << endl;
 
-                 << endl;
     }
+    cout << "What is your name prisoner?" << endl;
+    cin >> name;
+    cout << "Welcome " << name << " What is your race? Choose via the Number\n" << endl;
+    cout << "1.Dark Gnome\n"
+            "2.Galactical Mushroom\n"
+            "3.Ogre\n"
+            "4.Human\n"
+            "5.Ultimate Fish\n"
+            "6.Goblin\n" << endl;
+    cin >> race;
+
+
 
     return 0;
 }
